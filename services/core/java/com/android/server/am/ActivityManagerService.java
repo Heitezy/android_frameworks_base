f8f01e8e200dd017dde2cf6d2dd0dc27685a1a1a
Christopher Tate <ctate@google.com>
2019-08-19 16:16:20 -0700

    DO NOT MERGE - Kill apps outright for API contract violations
    ...rather than relying on in-app code to perform the shutdown.
    
    Backport of security fix.
    
    Bug: 128649910
    Bug: 140108616
    Test: manual
    Test: atest OsHostTests#testForegroundServiceBadNotification
    
    [basilgello: back-port to 14.1:
    - core/java/android/app/IActivityManager.aidl -> core/java/android/app/IActivityManager.java,
    - no serviceForegroundCrash in services/core/java/com/android/server/am/ActiveServices.java,
    - no runCrash in services/core/java/com/android/server/am/ActivityManagerShellCommand.java,
    - add argument to ActivityManagerProxy,
    - no mNotificationLock and ForegroundService,
    - adjust args count (remove '-1') in killMisbehavingService]
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>
    
    Change-Id: I94d9de50bb03c33666471e3dbd9c721e9278f7cb
    Merged-In: I94d9de50bb03c33666471e3dbd9c721e9278f7cb
    (cherry picked from commit a79b6ba5c59dc6aaa8adbe1ffa3ee4b761f45e7f)

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5144,7 +5144,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
     @Override
     public void crashApplication(int uid, int initialPid, String packageName,
-            String message) {
+            String message, boolean force) {
         if (checkCallingPermission(android.Manifest.permission.FORCE_STOP_PACKAGES)
                 != PackageManager.PERMISSION_GRANTED) {
             String msg = "Permission Denial: crashApplication() from pid="
@@ -5156,7 +5156,8 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
 
         synchronized(this) {
-            mAppErrors.scheduleAppCrashLocked(uid, initialPid, packageName, message);
+            mAppErrors.scheduleAppCrashLocked(uid, initialPid, packageName,
+                    message, force);
         }
     }
 
